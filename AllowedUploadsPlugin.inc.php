c15e771029869c138d9193ed76a495e7c68668f4
ajnyga <ajnyga@gmail.com>
2018-07-27 17:28:26 +0300

    update license to GPL3

--- AllowedUploadsPlugin.inc.php
+++ AllowedUploadsPlugin.inc.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2014-2018 Simon Fraser University
  * Copyright (c) 2003-2018 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class AllowedUploadsPlugin
  * @ingroup plugins_generic_allowedUploads
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
  *
  * Copyright (c) 2014-2018 Simon Fraser University
  * Copyright (c) 2003-2018 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_allowedUploads
  * @brief Wrapper for Allowed Uploads plugin.
